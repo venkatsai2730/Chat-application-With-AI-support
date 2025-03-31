bb186c36a911ee027f7fee78e73937276e9442a8
Venkat Sai Anil Kumar Thammisetty <101638363+venkatsai2730@users.noreply.github.com>
2025-03-31 17:29:24 +0530

    Update Project.jsx

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -5,7 +5,7 @@ import axios from '../config/axios'
 import { initializeSocket, receiveMessage, sendMessage } from '../config/socket'
 import Markdown from 'markdown-to-jsx'
 import hljs from 'highlight.js';
-import { getWebContainer } from '../config/webcontainer'
+import { getWebContainer } from '../config/webContainer'
 
 
 
