a0b9daf1829d4ee678cd54a7b93420181942a1f5
Venkat Sai Anil Kumar Thammisetty <101638363+venkatsai2730@users.noreply.github.com>
2025-04-01 11:29:02 +0530

    Update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
-import Login from '../screens/Login'
-import Register from '../screens/Register'
-import Home from '../screens/Home'
-import Project from '../screens/Project'
-import UserAuth from '../auth/UserAuth'
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Login from "../screens/Login";
+import Register from "../screens/Register";
+import Home from "../screens/Home";
+import Project from "../screens/Project";
+import UserAuth from "../auth/UserAuth";
 
 const AppRoutes = () => {
     return (
-        <BrowserRouter>
-
+        <Router basename="/">
             <Routes>
                 <Route path="/" element={<UserAuth><Home /></UserAuth>} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/project" element={<UserAuth><Project /></UserAuth>} />
             </Routes>
+        </Router>
+    );
+};
 
-        </BrowserRouter>
-    )
-}
-
-export default AppRoutes
+export default AppRoutes;
